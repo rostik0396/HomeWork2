74eeee192a9de7ac642458dc7cc9587c969547e0
rostik0396 <80713641+rostik0396@users.noreply.github.com>
2021-03-21 18:39:37 +0200

    Howe Work 2

---
+++ index1.js
@@ -0,0 +1,19 @@
+var message, 
+message2,
+userFirstName, 
+userSecondName,
+userAge; 
+
+message = "Hello my name is";
+message2 = "and i am";
+message3 = "years old";
+userFirstName = "Rosticlav";
+userSecondName = "Cherviakov";
+userAge = 25;
+
+var helloMessage = message + ' ' + userFirstName + ' ' + userSecondName + ' ' + message2 + ' ' + userAge + ' ' + message3;
+
+console.log(helloMessage);
+
+
+
